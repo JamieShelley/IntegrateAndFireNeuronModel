01f6c9c26914e6c5aa8205be511f5ece2da1b3c3
sirshelley <sirshelley@hotmail.co.uk>
2018-05-16 03:03:19 +0200

    Add dll test and dll reference - console app

---
+++ DLLTest/Dll1/Dll1/MathLibrary.h
@@ -0,0 +1,26 @@
+//Src: https://msdn.microsoft.com/en-us/library/ms235636.aspx
+#pragma once
+
+
+#ifdef MATHLIBRARY_EXPORTS  
+#define MATHLIBRARY_API __declspec(dllexport)   
+#else  
+#define MATHLIBRARY_API __declspec(dllimport)   
+#endif  
+
+namespace MathLibrary
+{
+	// This class is exported from the MathLibrary.dll  
+	class Functions
+	{
+	public:
+		// Returns a + b  
+		static MATHLIBRARY_API double Add(double a, double b);
+
+		// Returns a * b  
+		static MATHLIBRARY_API double Multiply(double a, double b);
+
+		// Returns a + (a * b)  
+		static MATHLIBRARY_API double AddMultiply(double a, double b);
+	};
+}
